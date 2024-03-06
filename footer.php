7446b05fe7591d0c7b7852e94f07261ef35e0914
ing-alexsipenaranda <130710184+ing-alexsipenaranda@users.noreply.github.com>
2024-03-06 17:38:09 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         <div class="contenedor contenedor-footer">
             <nav class="navegacion">
                 <a href="nosotros.php">Nosotros</a>
-                <a href="anuncios.php">Prepagos</a>
+                <a href="anuncios.php">Casas</a>
                 <a href="blog.php">Blog</a>
                 <a href="contacto.php">Contacto</a>
             </nav>
