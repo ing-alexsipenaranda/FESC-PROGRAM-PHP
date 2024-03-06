c10c3f76c6024d62728a469119f686aabc8cdbab
ing-alexsipenaranda <130710184+ing-alexsipenaranda@users.noreply.github.com>
2024-03-06 17:36:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 
 <section class="imagen-contacto">
-        <h2 class="index">Encuentra la Web Cam de tus sueños</h2>
+        <h2 class="index">Encuentra la CASA de tus sueños</h2>
         <p class="index">Llena el formulario de contacto y una colaboradora se comunicara contigo</p>
         <a href="contacto.php" class="boton-amarillo">Contactanos</a>
 </section>
